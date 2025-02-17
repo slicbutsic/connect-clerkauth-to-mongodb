badcc6b97b3c91ced2b761aa711b73fffe84b43b
Lucas Siviglia <siviglialucas@gmail.com>
2025-02-18 09:56:17 +1000

    fix user actions

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,13 +1,40 @@
+// 'use server';
+// import User from '@/lib/modals/user.modal';
+// import { connect } from "@/lib/db";
+
+// export async function createUser(user: any) {
+//   try {
+//     await connect();
+//     const newUser = new User(user);
+//     return JSON.parse(JSON.stringify(newUser));
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+
 'use server';
 import User from '@/lib/modals/user.modal';
 import { connect } from "@/lib/db";
 
-export async function createUser(user: any) {
+interface UserData {
+  clerkId: string;
+  email: string;
+  password: string;
+  photo: string;
+  username?: string;
+  firstName?: string;
+  lastName?: string;
+}
+
+export async function createUser(user: UserData) {
   try {
     await connect();
     const newUser = new User(user);
+    await newUser.save();
     return JSON.parse(JSON.stringify(newUser));
   } catch (err) {
-    console.error(err);
+    console.error("Error creating user:", err);
+    throw err;
   }
 }
