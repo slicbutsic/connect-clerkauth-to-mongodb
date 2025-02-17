5a5e026f4a200fca0214c158bb697dcaa1d70e2f
Lucas Siviglia <siviglialucas@gmail.com>
2025-02-18 09:58:50 +1000

    fix clerk webhook

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,11 +1,12 @@
 // import { clerkClient } from '@clerk/nextjs';
 // import { WebhookEvent } from '@clerk/nextjs/server';
 // import { headers } from 'next/headers';
-// import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 // import { Webhook } from 'svix';
 
 // import { createUser } from '@/lib/actions/user.action'
 
-// export async function POST(req: Request) {
-
-// }
+export async function POST(req: Request) {
+  // Your webhook handling logic here
+  return NextResponse.json({ message: 'Webhook received' });
+}
