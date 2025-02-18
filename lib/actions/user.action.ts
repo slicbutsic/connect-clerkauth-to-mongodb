e11c8ea5fa5f4a81c39c7c1973022fbe9eb01db8
Lucas Siviglia <siviglialucas@gmail.com>
2025-02-18 11:32:03 +1000

    export interface userdata

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,6 +1,30 @@
+// 'use server';
+// import User from '@/lib/modals/user.modal';
+// import { connect } from "@/lib/db";
+
+// export async function createUser(user: any) {
+//   try {
+//     await connect();
+//     const newUser = new User(user);
+//     return JSON.parse(JSON.stringify(newUser));
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
 'use server';
 import User from '@/lib/modals/user.modal';
 import { connect } from "@/lib/db";
+
+export interface UserData {
+  clerkId: string;
+  email: string;
+  password: string;
+  photo: string;
+  username?: string;
+  firstName?: string;
+  lastName?: string;
+}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function createUser(user: any) {
@@ -12,4 +36,3 @@ export async function createUser(user: any) {
     console.error(err);
   }
 }
-
