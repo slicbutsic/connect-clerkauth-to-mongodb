7a7b50a5c3a9e0e4c6cc3d7600bbe74993599e99
Lucas Siviglia <siviglialucas@gmail.com>
2025-02-18 12:45:22 +1000

    change clerk connection

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,21 +2,11 @@
 import User from '@/lib/modals/user.modal';
 import { connect } from "@/lib/db";
 
-// export interface UserData {
-//   clerkId: string;
-//   email: string;
-//   password: string;
-//   photo: string;
-//   username?: string;
-//   firstName?: string;
-//   lastName?: string;
-// }
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function createUser(user: any) {
   try {
     await connect();
-    const newUser = new User(user);
+    const newUser = await User.create(user);
     return JSON.parse(JSON.stringify(newUser));
   } catch (err) {
     console.error(err);
