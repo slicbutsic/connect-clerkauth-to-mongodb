9f4c6f2128cfda4249abf5ddd45c15e34b8bb6ed
Lucas Siviglia <siviglialucas@gmail.com>
2025-02-18 11:29:13 +1000

    change user action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,40 +1,15 @@
-// 'use server';
-// import User from '@/lib/modals/user.modal';
-// import { connect } from "@/lib/db";
-
-// export async function createUser(user: any) {
-//   try {
-//     await connect();
-//     const newUser = new User(user);
-//     return JSON.parse(JSON.stringify(newUser));
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
-
-
 'use server';
 import User from '@/lib/modals/user.modal';
 import { connect } from "@/lib/db";
 
-export interface UserData {
-  clerkId: string;
-  email: string;
-  password: string;
-  photo: string;
-  username?: string;
-  firstName?: string;
-  lastName?: string;
-}
-
-export async function createUser(user: UserData) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function createUser(user: any) {
   try {
     await connect();
     const newUser = new User(user);
-    await newUser.save();
     return JSON.parse(JSON.stringify(newUser));
   } catch (err) {
-    console.error("Error creating user:", err);
-    throw err;
+    console.error(err);
   }
 }
+
--- lib/db.ts
+++ lib/db.ts
@@ -1,48 +1,4 @@
 // import mongoose, { Mongoose } from 'mongoose';
-
-// const MONGODB_URL = process.env.MONGODB_URL as string;
-
-// interface MongooseConn {
-//   conn: Mongoose | null;
-//   promise: Promise<Mongoose> | null;
-// }
-
-// // Extend the global object to include our mongoose property
-// declare global {
-//   // eslint-disable-next-line no-var
-//   var mongoose: MongooseConn | undefined;
-// }
-
-// const cached: MongooseConn = global.mongoose || {
-//   conn: null,
-//   promise: null
-// };
-
-// if (!global.mongoose) {
-//   global.mongoose = cached;
-// }
-
-// export const connect = async (): Promise<Mongoose> => {
-//   if (cached.conn) return cached.conn;
-
-//   if (!cached.promise) {
-//     cached.promise = mongoose.connect(MONGODB_URL, {
-//       dbName: 'connecting-clerk-to-mongodb',
-//       bufferCommands: false,
-//       connectTimeoutMS: 30000,
-//     });
-//   }
-
-//   try {
-//     cached.conn = await cached.promise;
-//   } catch (e) {
-//     cached.promise = null;
-//     throw e;
-//   }
-
-//   return cached.conn;
-// };
-
 import mongoose from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL!;
